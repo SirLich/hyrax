0818cb48828d16ec8b952dd76735e5bacc13ad2f
SirLich <lrkoehler77@gmail.com>
2023-11-11 23:55:15 +0100

    Integrate serde

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,38 @@
 use clap::{Parser, Subcommand};
-use std::{fs, io, env, path};
+use std::{env, path};
+use std::fs::{self, File};
+use std::io::{self, Read, Write};
 use anyhow::{Context, Result, bail};
+use serde::{Deserialize, Serialize};
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Root {
+    pub foo: i64,
+
+    // Ignore unknown fields
+    #[serde(flatten)]
+    unknown: serde_json::Map<String, serde_json::Value>,
+}
+
+fn read_config() -> Result<Root> {
+    let mut file = File::open("config.json")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let config: Root = serde_json::from_str(&contents)?;
+
+    Ok(config)
+}
+
+fn write_config(config: &Root) -> Result<()> {
+    let mut file = File::create("config.json")?;
+    let json_str = serde_json::to_string_pretty(config)?;
+
+    file.write_all(json_str.as_bytes())?;
+    Ok(())
+}
+
 
 /// A lite re-implementation of Regolith in Rust.
 #[derive(Parser, Debug)]
@@ -21,6 +53,9 @@ enum Command {
     Run {
 
     },
+    Test {
+
+    }
 }
 
 fn is_directory_empty(path: path::PathBuf) -> Result<bool> {
@@ -51,6 +86,12 @@ fn main() {
         },
         Command::Run { } => {
             println!("Regolith Run!")
+        }
+        Command::Test {} => {
+            let mut config = read_config().expect("Could not read config");
+            config.foo = 200;
+
+            write_config(&config).expect("Could not write config.");
         }
     }
 }
